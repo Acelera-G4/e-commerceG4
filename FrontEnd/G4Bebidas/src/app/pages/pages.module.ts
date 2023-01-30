9c460e2e05588eeec658e021ec7d1a8b2a1c0921
William Filgueira dos Santos <extern.william.santos@volkswagen.com.br>
2023-01-29 21:13:57 -0300

    created cards

--- FrontEnd/G4Bebidas/src/app/pages/pages.module.ts
+++ FrontEnd/G4Bebidas/src/app/pages/pages.module.ts
@@ -1,35 +1,27 @@
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './home/home.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { FooterComponent } from './components/footer/footer.component';
-
-
-
-
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    CardComponent,
   ],
-  imports: [
-    CommonModule,
-    MatToolbarModule,
-    MatIconModule  
-    
-    
-  ],
+  imports: [CommonModule, MatToolbarModule, MatIconModule],
   exports: [
     HeaderComponent,
     HomeComponent,
     MatToolbarModule,
     MatIconModule,
-    FooterComponent
-  ]
+    FooterComponent,
+    CardComponent
+  ],
 })
-export class PagesModule { }
+export class PagesModule {}
