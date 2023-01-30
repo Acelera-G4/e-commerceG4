63ebc1904eb6daa585cd454675821a1b09a15782
William Filgueira dos Santos <extern.william.santos@volkswagen.com.br>
2023-01-29 23:18:40 -0300

    create routes

--- FrontEnd/G4Bebidas/src/app/app.module.ts
+++ FrontEnd/G4Bebidas/src/app/app.module.ts
@@ -3,26 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PagesModule } from './pages/pages.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FooterComponent } from './pages/components/footer/footer.component';
+import { HeaderComponent } from './pages/components/header/header.component';
+import { CardComponent } from './pages/components/card/card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-   
+    HeaderComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PagesModule,
     BrowserAnimationsModule,
-
+    
+   
   ],
-  exports: [
-
-  ],
+  exports: [AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
