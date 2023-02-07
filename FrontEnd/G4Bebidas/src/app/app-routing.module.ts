618ca6bffe7a80037d205b227b796c339adf1780
William Filgueira dos Santos <extern.william.santos@volkswagen.com.br>
2023-02-06 22:41:56 -0300

    create route to dashboard, user list and list products

--- FrontEnd/G4Bebidas/src/app/app-routing.module.ts
+++ FrontEnd/G4Bebidas/src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { ProductsComponent } from './pages/components/products/products.component';
+import { UserComponent } from './pages/components/user/user.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -7,10 +10,13 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent },
   {path: "", component: HomeComponent },
+  {path: 'home', component: HomeComponent },
   {path: 'login', component: LoginComponent},
   {path: 'sign-up', component: SignUpComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'product', component: ProductsComponent},
 ];
 
 @NgModule({
