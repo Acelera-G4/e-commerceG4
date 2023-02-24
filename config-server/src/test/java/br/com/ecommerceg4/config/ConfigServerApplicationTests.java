a42b2d94ab5307f0e95feba9b488eb3ed8bf5b9d
Gabriel <gvscceca@gmail.com>
2023-02-24 19:17:52 -0300

    services config

---
+++ config-server/src/test/java/br/com/ecommerceg4/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecommerceg4.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ service-discovery/src/test/java/br/com/ecommerceg4/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecommerceg4.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
