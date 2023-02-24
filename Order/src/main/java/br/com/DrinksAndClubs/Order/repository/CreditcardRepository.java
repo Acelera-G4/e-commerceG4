5c16698097fa1855dcbd0956ed9ab0a6049906e9
Alex Souza <alexsandrodse@gmail.com>
2023-02-24 17:13:32 -0300

    ECAG-30 <criação da classe Creditcard>

---
+++ Order/src/main/java/br/com/DrinksAndClubs/Order/repository/CreditcardRepository.java
@@ -0,0 +1,15 @@
+package br.com.DrinksAndClubs.Order.repository;
+
+import br.com.DrinksAndClubs.Order.model.Creditcard;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CreditcardRepository extends JpaRepository<Creditcard, Long> {
+
+    @Query("select * from tb_credit-card where name like %?%")
+    List<Creditcard> findAllByName(String name);
+}
