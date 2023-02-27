f7095015e69fabef1e6166590edf3e79b4788e2b
Gabriel <gvscceca@gmail.com>
2023-02-27 15:06:57 -0300

    modified configurations

--- API-Users/src/main/java/com/br/atos/APIUsers/ApiUsersApplication.java
+++ API-Users/src/main/java/com/br/atos/APIUsers/ApiUsersApplication.java
@@ -2,14 +2,17 @@ package com.br.atos.APIUsers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletPath;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //@EnableWebSecurity
 @SpringBootApplication
+@EnableEurekaClient
 public class ApiUsersApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiUsersApplication.class, args);
-		System.out.println("Bom dia");
-	}
+	public static void main(String[] args) {SpringApplication.run(ApiUsersApplication.class, args);	}
 
 }
--- gateway/src/main/java/br/com/ecommerceg4/gateway/GatewayApplication.java
+++ gateway/src/main/java/br/com/ecommerceg4/gateway/GatewayApplication.java
@@ -2,12 +2,23 @@ package br.com.ecommerceg4.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+		return builder.routes()
+				.route(r -> r.path("/users/**").uri("http://localhost:8095/"))
+				.route(r -> r.path("/productapi/**").uri("http://localhost:8091/"))
+				.build();
 	}
 
 }
